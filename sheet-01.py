57feb7b33d32648cee67f4d77eda350266dfbdee
DominiNieder <dominic.nieder@gmail.com>
2024-10-21 19:09:12 +0200

    Implementation of collision -> Test?

--- sheet-01.py
+++ sheet-01.py
@@ -2,38 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Particle:
-    def __init__(self, r, x, y, vx, vy):
-        self.r=r
-        self.x=x
-        self.y=y
-        self.vx=vx
-        self.vy=vy
+    def __init__(self, r:float, x:float, y:float, vx:float, vy:float):
+        self.r:float=r
+        self.x:float=x
+        self.y:float=y
+        self.vx:float=vx
+        self.vy:float=vy
         
     def __repr__(self):
          return(str("This is a particle at %0.2f, %0.2f with v=%0.2f,%0.2f" % (self.x,self.y,self.vx,self.vy)) )
 
-# p2 -> p1
-def distance(p1,p2):  
-    return(p1.x-p2.x , p1.y-p2.y)
-
-def collision(p1, p2):
-    d21=distance(p1,p2)  # distance pointing to p1
-    e21=d21/np.sqrt(d21[0]**2 + d21[1]**2)
-    theta=np.arctan(d21[1]/d21[0])
-    v_rel=(p2.vx-p1.vx, p2.vy-p1.vx)
-    vx1=np.cos(theta)*p2.vx
-    vy1=np.cos(theta)*p2.vy
-    return(vx1, vy1)
+# p2 ->p1
+def distance(p1:Particle,p2:Particle) -> tuple[float]:  
+    return((p1.x-p2.x) , (p1.y-p2.y))
 
 
 def run():
     box=(20,20)
-    n=2
-    r=1.0
-    v0=0.5
+    n:int=2
+    r:float=1.0
+    v0:float=0.5
     # angles=np.random.rand(n)*2*np.pi
-    angles=np.array([0, np.pi/2])
-    par=[Particle(
+    angles:list[float]=np.array([0, np.pi/2])
+    par:list[Particle]=[Particle(
             r,
             10.0+i,
             10.0,
@@ -44,24 +35,23 @@ def run():
             )
         for i in range(n)]    
 
-    T=50
+    T:int=50
     x_data=np.zeros((n,T))
     y_data=np.zeros((n,T))
     vx_data=np.zeros((n,T))
     vy_data=np.zeros((n,T))
-    print(type(x_data))
-    print(type(x_data[0]))
-    x_data[0][0]=1.0
-    print(x_data[0][0])
-    dt=1.0
+    dt:float=1.0
 
     for t in range(T):
-        for i in range(n):  # iteration
-            for j in range(i,n):
-                dvx_ij = par[j].vx - par[i].vx
-                dvy_ij = par[j].vy - par[i].vy
-                par[i].vx=par[i].vx
-                par[i].vy=par[i].vy
+        for i in range(n):  # iteration over all particles
+            for j in range(i+1,n):  # iteration over all others
+                d_vec = distance(par[i],par[j])  # distance vec j -> i
+                d_abs = d_vec[0]**2+d_vec[1]**2  # distance
+                d_unit = d_vec/np.sqrt(d_abs)  # dist direct
+                if d_abs <= 4*par[0].r**2:  # condition for collision
+                    dvix,dviy= -((par[i].vx-par[j].vx)*d_unit[0]+ (par[i].vy-par[j].vy)*d_unit[1]) *d_unit
+                    par[i].vx, par[i].vy= par[i].vx+dvix , par[i].vy+dviy
+                    par[j].vx, par[j].vy= par[j].vy-dviy , par[j].vy-dviy
 
             par[i].x = (par[i].x+par[i].vx*dt) % box[0] 
             par[i].y = (par[i].y+par[i].vy*dt) % box[1]
@@ -78,6 +68,23 @@ def run():
     plt.legend()
     plt.savefig("relative-position-2p-system_5.png")
     
+
+def diffGeo(c):
+    x=np.linspace(-10,10,200)
+    y=np.linspace(-10,10,200)
+    x_plot=[]
+    y_plot=[]
+    for i in x:
+        for j in y:
+            if i**2-j**2==c:
+                x_plot.append(i)
+                y_plot.append(j)
+
+
+
+    plt.plot(x_plot,y_plot)
+    plt.savefig("diffgeo2.png")
+
 
 if __name__ == "__main__":
     run()
