ef287b93b2f5d065dcf757fd2e05b5636346b167
DominiNieder <dominic.nieder@gmail.com>
2024-10-22 14:17:54 +0200

    System shift:S->S' for nearest neighbour search,
    korrekt testing results

--- sheet-01.py
+++ sheet-01.py
@@ -2,38 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Particle:
-    def __init__(self, r:float, x:float, y:float, vx:float, vy:float):
-        self.r:float=r
-        self.x:float=x
-        self.y:float=y
-        self.vx:float=vx
-        self.vy:float=vy
+    def __init__(self, r:float|int, x:float|int, y:float|int, vx:float|int, vy:float|int):
+        self.r:float|int=r
+        self.x:float|int=x
+        self.y:float|int=y
+        self.vx:float|int=vx
+        self.vy:float|int=vy
         
     def __repr__(self):
          return(str("This is a particle at %0.2f, %0.2f with v=%0.2f,%0.2f" % (self.x,self.y,self.vx,self.vy)) )
 
-# p2 ->p1
-def distance(p1:Particle,p2:Particle) -> tuple[float]:  
-    return((p1.x-p2.x) , (p1.y-p2.y))
+### shift to S->S' of which p1'=(box[0]/2, box[1]/2)
+def system_shift(p1:Particle,box:tuple[float|int]) -> tuple[float|int]:
+    (dx,dy) = (box[0]/2-p1.x , box[1]/2-p1.y)
+    return(dx,dy)
 
+### vec(p2->p1)
+def rel_koordiantes(p1:Particle,p2:Particle,box:tuple[float|int]) -> tuple[float|int]:  
+    dx, dy = system_shift(p1,box)  # S->S': S' is a box with p1' at its center
+    print("10,10=",p1.x+dx,p1.y+dy)
+    print("p2'=",(p2.x+dx)%box[0], (p2.y+dy)%box[1])
+    return(p1.x - (p2.x+dx)%box[0] , p1.y - (p2.y+dy)%box[1])
 
 def run():
-    box=(20,20)
+    box:tuple[float|int]=(20,20)
     n:int=2
-    r:float=1.0
-    v0:float=0.5
-    # angles=np.random.rand(n)*2*np.pi
-    angles:list[float]=np.array([0, np.pi/2])
+    r:float|int=1.0
+    v0:float|int=0.5
+    angles=np.random.rand(n)*2*np.pi
+    #  par:list[Particle]=[Particle(
+    #          r,
+    #          np.random.randint(1,box[0]-1,1)[0],
+    #          np.random.randint(1,box[1]-1,1)[0],
+    #          v0*np.cos(angles[i]),
+    #          v0*np.sin(angles[i])
+    #          )
+    #      for i in range(n)]   
     par:list[Particle]=[Particle(
-            r,
-            10.0+i,
-            10.0,
-            # np.random.randint(1,box[0]-1,1)[0],
-            # np.random.randint(1,box[1]-1,1)[0],
-            v0*np.cos(angles[i]),
-            v0*np.sin(angles[i])
-            )
-        for i in range(n)]    
+        1.0,
+        0.0,
+        0.0,
+        2.0,
+        0.0
+    ), Particle(
+        1.0,
+        0.0,
+        18.0,
+        0.0,
+        0.0
+    )] 
 
     T:int=50
     x_data=np.zeros((n,T))
@@ -41,18 +58,26 @@ def run():
     vx_data=np.zeros((n,T))
     vy_data=np.zeros((n,T))
     dt:float=1.0
-
+    ### Simulation
     for t in range(T):
-        for i in range(n):  # iteration over all particles
+        ### iterate all particles
+        for i in range(n):
+            print("iter i=",i)
+            ### collision
             for j in range(i+1,n):  # iteration over all others
-                d_vec = distance(par[i],par[j])  # distance vec j -> i
-                d_abs = d_vec[0]**2+d_vec[1]**2  # distance
-                d_unit = d_vec/np.sqrt(d_abs)  # dist direct
-                if d_abs <= 4*par[0].r**2:  # condition for collision
-                    dvix,dviy= -((par[i].vx-par[j].vx)*d_unit[0]+ (par[i].vy-par[j].vy)*d_unit[1]) *d_unit
-                    par[i].vx, par[i].vy= par[i].vx+dvix , par[i].vy+dviy
-                    par[j].vx, par[j].vy= par[j].vy-dviy , par[j].vy-dviy
-
+                print("iter j=", j)
+                d_vec:tuple[float|int] = rel_koordiantes(par[i],par[j], box)  # rel. distance vector j -> i
+                d_abs = d_vec[0]**2+d_vec[1]**2  # distance squared
+                print("distance=",d_abs)
+                #### collision condition -> kinematics
+                if np.sqrt(d_abs) <= par[i].r+par[j].r:
+                    d_unit:tuple[float|int] = d_vec/np.sqrt(d_abs)  # collision axis
+                    print("!collision!: Axis",d_unit)
+                    dvix , dviy= -((par[i].vx-par[j].vx)*d_unit[0]+ (par[i].vy-par[j].vy)*d_unit[1]) *d_unit
+                    print("dv=",(dvix, dviy))
+                    par[i].vx , par[i].vy = (par[i].vx+dvix) , ( par[i].vy+dviy)
+                    par[j].vx , par[j].vy = (par[j].vy-dviy) , (par[j].vy-dviy)
+            print("particle",i ," velocities:",par[i].vx,par[i].vy)
             par[i].x = (par[i].x+par[i].vx*dt) % box[0] 
             par[i].y = (par[i].y+par[i].vy*dt) % box[1]
             x_data[i][t]=par[i].x
@@ -60,7 +85,7 @@ def run():
             vx_data[i][t]=par[i].vx
             vy_data[i][t]=par[i].vy
 
-    plt.plot(x_data[0]-x_data[1],y_data[0]-y_data[1],label="relative")
+    #plt.plot(x_data[0]-x_data[1],y_data[0]-y_data[1],label="relative")
     plt.plot(x_data[0],y_data[0],label="p1")
     plt.plot(x_data[1],y_data[1],label="p2")
     plt.xlabel("x [nm]")
@@ -68,23 +93,6 @@ def run():
     plt.legend()
     plt.savefig("relative-position-2p-system_5.png")
     
-
-def diffGeo(c):
-    x=np.linspace(-10,10,200)
-    y=np.linspace(-10,10,200)
-    x_plot=[]
-    y_plot=[]
-    for i in x:
-        for j in y:
-            if i**2-j**2==c:
-                x_plot.append(i)
-                y_plot.append(j)
-
-
-
-    plt.plot(x_plot,y_plot)
-    plt.savefig("diffgeo2.png")
-
 
 if __name__ == "__main__":
     run()
